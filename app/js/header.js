bae6cf109c0dda1f450147a91896bcc25a0c9c2f
NKC <khacchang128@gmail.com>
2022-07-08 16:26:09 +0700

    new contact

--- app/js/header.js
+++ app/js/header.js
@@ -1,59 +1,9 @@
 const ZALO = "https://zalo.me/562398999";
 const FACEBOOK = "https://www.facebook.com/profile.php?id=100064734504163";
-const PHONE = "0562398999";
+const PHONE = "0562.398.999";
+const PHONECALL = "0562398999";
 const MAIL = "nguyenvanhung28686@gmail.com";
-
-//back to top
-const btnBackToTop = document.getElementById("back-to-top");
-const scrollFunction = () => {
-  if (
-    document.body.scrollTop > 400 ||
-    document.documentElement.scrollTop > 400
-  ) {
-    btnBackToTop.style.display = "block";
-  } else {
-    btnBackToTop.style.display = "none";
-  }
-};
-window.onscroll = function () {
-  scrollFunction();
-};
-const backToTop = () => {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-};
-btnBackToTop.addEventListener("click", backToTop);
-
-//contact
-const zalo = document.getElementById("zalo");
-const facebook = document.getElementById("facebook");
-const phone = document.getElementById("phone");
-const website = document.getElementById("website");
-//footer
-const facebookFooter = document.getElementById("facebook-footer");
-const zaloFooter = document.getElementById("zalo-footer");
-const mailFooter = document.getElementById("mail-footer");
-const phoneFooter = document.getElementById("phone-footer");
-//
-website.innerHTML = `Website: <a href="#">https://kdtthanhha.com</a>`;
-facebookFooter.innerHTML = ` Faccbook: <a href=${FACEBOOK}><b>Nguyễn Văn Hùng</b></a>`;
-zaloFooter.innerHTML = `Zalo: <a href="${ZALO}"><b>${PHONE}</b></a>`;
-mailFooter.innerHTML = `Mail: <a ><b>nguyenvanhung28686@gmail.com</b></a>`;
-phoneFooter.innerHTML = `Hotline: ${PHONE}`;
-//Lienhe
-// const facebooklh = document.getElementById("facebook-lh");
-// const zalolh = document.getElementById("zalo-lh");
-// const maillh = document.getElementById("mail-lh");
-// const phonelh = document.getElementById("phone-lh");
-// //
-// //facebooklh.innerHTML = `Facebook: <a href=${FACEBOOK} class="facebook">Nguyễn Văn Hùng</a>`;
-// console.log(facebooklh);
-// phonelh.innerHTML = `Hotline: <a href="" class="hotline">${PHONE} </a>`;
-// maillh.innerHTML = `Email: <a href="" class="mail">${MAIL}</a>`;
-
-zalo.innerHTML = `<a href=${ZALO}>Chat Zalo</a>`;
-facebook.innerHTML = `<a href=${FACEBOOK}>Chat Facebook</a>`;
-phone.innerHTML = `<a>Hotline: ${PHONE}</a>`;
+const MAP = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14906.05517069742!2d105.77527041626236!3d20.931872211441487!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ad494e8f8521%3A0x9d70aa1dd7ac2e54!2zVGhhbmggSMOgIE3GsOG7nW5nIFRoYW5oIENpZW5jbyA1!5e0!3m2!1svi!2s!4v1657262025127!5m2!1svi!2s" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
 
 //header
 const header = document.getElementById("header");
@@ -96,3 +46,64 @@ closeSidebar.addEventListener("click", () => {
   overlay.classList.remove("active");
   sidebar.classList.remove("show-sidebar");
 });
+
+//back to top
+const btnBackToTop = document.getElementById("back-to-top");
+const scrollFunction = () => {
+  if (
+    document.body.scrollTop > 400 ||
+    document.documentElement.scrollTop > 400
+  ) {
+    btnBackToTop.style.display = "block";
+  } else {
+    btnBackToTop.style.display = "none";
+  }
+};
+window.onscroll = function () {
+  scrollFunction();
+};
+const backToTop = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
+btnBackToTop.addEventListener("click", backToTop);
+
+//contact button
+const zalo = document.getElementById("zalo");
+const facebook = document.getElementById("facebook");
+const phone = document.getElementById("phone");
+zalo.innerHTML = `<a href=${ZALO}>Chat Zalo</a>`;
+facebook.innerHTML = `<a href=${FACEBOOK}>Chat Facebook</a>`;
+phone.innerHTML = `<a href="tel:${PHONECALL}">Hotline: ${PHONE}</a>`;
+
+//footer
+const website = document.getElementById("website");
+const facebookFooter = document.getElementById("facebook-footer");
+const zaloFooter = document.getElementById("zalo-footer");
+const mailFooter = document.getElementById("mail-footer");
+const phoneFooter = document.getElementById("phone-footer");
+website.innerHTML = `Website: <a href="#">https://kdtthanhha.com</a>`;
+facebookFooter.innerHTML = ` Faccbook: <a href=${FACEBOOK}><b>Nguyễn Văn Hùng</b></a>`;
+zaloFooter.innerHTML = `Zalo: <a href="${ZALO}"><b>${PHONE}</b></a>`;
+mailFooter.innerHTML = `Mail: <a ><b>nguyenvanhung28686@gmail.com</b></a>`;
+phoneFooter.innerHTML = `Hotline: ${PHONE}`;
+
+//Lienhe
+const facebooklh = document.querySelector(".facebook-lh");
+const zalolh = document.querySelector(".zalo-lh");
+const maillh = document.querySelector(".mail-lh");
+const phonelh = document.querySelector(".phone-lh");
+if (facebooklh) {
+  facebooklh.innerHTML = `Facebook: <a href=${FACEBOOK} class="social-acc">Nguyễn Văn Hùng</a>`;
+  phonelh.innerHTML = `Hotline: <a href="tel:${PHONECALL}" class="hotline">${PHONE} </a>`;
+  maillh.innerHTML = `Email: <a href="${MAIL}" class="mail">${MAIL}</a>`;
+  zalolh.innerHTML = `Zalo: <a href="${ZALO}" class="social-acc">Mr Hùng: ${PHONE}</a>`;
+}
+
+//map
+const mapIframe = document.querySelector(".map-iframe");
+if (mapIframe) mapIframe.innerHTML = MAP;
+
+//hotline-intro
+const hotlineIntro = document.querySelector(".hotline-intro");
+if (hotlineIntro) hotlineIntro.innerHTML = `Hotline: ${PHONE}`;
