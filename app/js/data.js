36422aaf9b30b5136839f38c6be26913c84e1283
NKC <khacchang128@gmail.com>
2022-08-08 20:16:21 +0700

    del 2 news post

--- app/js/data.js
+++ app/js/data.js
@@ -9,6 +9,7 @@ export const COMPANY = "KDT Thanh Hà - Mường Thanh Cienco 5";
 
 export const NewsListData = [
   {
+    name: "5-yeu-to-tao-nen-ten-tuoi-khu-do-thi-thanh-ha",
     img: "./assests/tienichnoikhu.jpg",
     title: "5 YẾU TỐ VÀNG TẠO NÊN TÊN TUỔI KHU ĐÔ THỊ THANH HÀ CIENCO 5",
     desc: "Nếu bạn là “dân” đầu tư bất động sản thì chắc chắn không thể nào ...",
@@ -20,13 +21,14 @@ export const NewsListData = [
     desc: "Khu đô thị Thanh Hà đang là cái tên vô cùng hot trong thị trường ...",
     link: "./vi-tri-khu-do-thi-thanh-ha-chu-dau-tu-tap-doan-muong-thanh.html",
   },
+  // {
+  //   img: "./assests/chudautu.jpg",
+  //   title: "TÌM HIỂU CHỦ ĐẦU TƯ KHU ĐÔ THỊ THANH HÀ",
+  //   desc: "Chủ đầu tư khu đô thị Thanh Hà là ai? Chủ đầu tư khu đô ...",
+  //   link: "./tim-hieu-chu-dau-tu-khu-do-thi-thanh-ha.html",
+  // },
   {
-    img: "./assests/chudautu.jpg",
-    title: "TÌM HIỂU CHỦ ĐẦU TƯ KHU ĐÔ THỊ THANH HÀ",
-    desc: "Chủ đầu tư khu đô thị Thanh Hà là ai? Chủ đầu tư khu đô ...",
-    link: "./tim-hieu-chu-dau-tu-khu-do-thi-thanh-ha.html",
-  },
-  {
+    name: "khong-nen-dau-tu-vao-bat-dong-san-thanh-ha-cienco5-neu-chua-biet-dieu-nay",
     img: "./assests/khongnen.jpg",
     title:
       "KHÔNG NÊN ĐẦU TƯ VÀO BẤT ĐỘNG SẢN THANH HÀ CIENCO 5 NẾU CHƯA BIẾT ĐIỀU NÀY",
@@ -34,18 +36,18 @@ export const NewsListData = [
     link: "./khong-nen-dau-tu-vao-bat-dong-san-thanh-ha-cienco5-neu-chua-biet-dieu-nay.html",
   },
   {
+    name: "7-tip-dau-tu-bat-dong-san-khu-do-thi-thanh-ha-khong-lo",
     img: "./assests/review-khu-do-thi-thanh-ha.jpg",
     title: "7 TIPS ĐẦU TƯ BẤT ĐỘNG SẢN KHU ĐÔ THỊ THANH HÀ KHÔNG LỖ",
     desc: "Bất động sản khu đô thị Thanh Hà đang là cái tên hot trong thị trường bất động sản ...",
     link: "./7-tip-dau-tu-bat-dong-san-khu-do-thi-thanh-ha-khong-lo.html",
   },
-
-  {
-    img: "./assests/biet-thu-thanh-ha-cienco-5.png",
-    title: "KIẾN TRÚC BIỆT THỰ THANH HÀ HÀ ĐÔNG CÓ GÌ ĐẶC BIỆT",
-    desc: "Khu đô thị Thanh Hà Hà Đông là một trong những khu đô thị xanh ...",
-    link: "./kien-truc-biet-thu-thanh-ha-ha-dong-co-gi-dac-biet.html",
-  },
+  // {
+  //   img: "./assests/biet-thu-thanh-ha-cienco-5.png",
+  //   title: "KIẾN TRÚC BIỆT THỰ THANH HÀ HÀ ĐÔNG CÓ GÌ ĐẶC BIỆT",
+  //   desc: "Khu đô thị Thanh Hà Hà Đông là một trong những khu đô thị xanh ...",
+  //   link: "./kien-truc-biet-thu-thanh-ha-ha-dong-co-gi-dac-biet.html",
+  // },
 ];
 
 export const Experience = [
