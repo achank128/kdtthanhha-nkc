cdd54385dddabe090edc6598527f6c60059701b4
NKC <khacchang128@gmail.com>
2022-11-18 15:30:42 +0700

    fix css product

--- app/js/product.js
+++ app/js/product.js
@@ -31,17 +31,23 @@ const setProduct = async () => {
             <ul>
               <li><b>Phân khu:</b> ${p.phan_khu}</li>
               <li><b>Mặt đường:</b> ${p.mat_duong}</li>
-              <li><b>Giá bán:</b> ${p.gia_ban} Triệu/m2</li>
+              <li><b>Giá bán:</b> ${
+                p.gia_ban ? p.gia_ban + "Triệu/m2" : "Liên hệ"
+              }</li>
             </ul>
             <ul>
               <li><b>Phân lô:</b> ${p.phan_lo}</li>
               <li><b>Diện tích:</b> ${p.dien_tich}</li>
-              <li><b>Tổng giá trị HĐ:</b> ${p.tong} Tỷ</li>
+              <li><b>Tổng giá trị HĐ:</b> ${
+                p.tong ? p.tong + "Tỷ" : "Liên hệ"
+              } </li>
             </ul>
             <ul>
-              <li><b>Ô số:</b> ${p.tong}</li>
+              <li><b>Ô số:</b> ${p.o_so}</li>
               <li><b>Ưu điểm:</b> ${p.uu_diem}</li>
-              <li><b>Điện thoại LH:</b> ${p.sdt}</li>
+              <li><b>Điện thoại liên hệ:</b> <a href="tel:${
+                p.sdt
+              }" style="color: red;">${p.sdt}</a></li>
             </ul>
           </div>
         </div>
